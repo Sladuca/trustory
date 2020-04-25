47d48a8b1a3cab9af1d694390741b96cffbd9dd1
Sebastien La Duca <sladuca777@gmail.com>
2020-04-25 18:06:32 -0400

    types

--- oracle/src/resolvers.ts
+++ oracle/src/resolvers.ts
@@ -1,17 +1,37 @@
+
+
+interface getCourseArgs {
+  id: string
+}
+
+interface CreateCourseArgs {
+  course: any
+}
+
+interface UpdateCourseArgs {
+  course: any
+}
+
+interface issueCertArgs {
+  courseId: string
+  recipient: string
+  recipientKey: string
+}
+
 export default {
   Query: {
-    getCourse: (id: string) => {
+    getCourse: (obj: any, { id }: getCourseArgs, context: any) => {
       return "unimplemented";
     }
   },
   Mutation: {
-    createCourse: (course: any) => {
+    createCourse: (obj: any, { course }: CreateCourseArgs, context: any) => {
       return "unimplemented";
     },
-    updateCourse: (course: any) => {
+    updateCourse: (obj: any, { course }: UpdateCourseArgs, context: any) => {
       return "unimplemented";
     } ,
-    issueCert: (courseId: string, recipient: string) => {
+    issueCert: (obj: any, { courseId, recipient, recipientKey }: issueCertArgs, context: any) => {
       return "unimplemented";
     }
   }
