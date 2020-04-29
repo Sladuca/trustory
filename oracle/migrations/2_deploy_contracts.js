71050038023f95a481a48699d5bc050e2b5e8b9e
Sebastien La Duca <sladuca777@gmail.com>
2020-04-29 16:20:00 -0400

    stuff

--- oracle/migrations/2_deploy_contracts.js
+++ oracle/migrations/2_deploy_contracts.js
@@ -4,5 +4,5 @@ const TrustoryOracle = artifacts.require("TrustoryOracle");
 
 module.exports = function(deployer) {
   deployer.deploy(Trustory);
-  deployer.deploy(TrustoryOracle, "0xD583F257c6DFC9d48AFC06E535383611AD24bA0a", "0x3F14Eb90071552021AC5d82e4f0Bde0fCe6C2c90");
+  deployer.deploy(TrustoryOracle, "ORACLE_ADDRESS", "TRUSTORY_CONTRACT_ADDRESS");
 };
